9dd7b6b4b11c0408da4784deed12ff67fb9cb7fa
Rim Rakhimov <rimrakhimov@gmail.com>
2024-05-06 12:44:46 +0300

    Add swagger pages for blockscout-ens, eth-bytecode-db, smart-contract-verifier

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,9 +1,10 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
+  let service_name = window.document.title
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "swagger.yaml",
+    url: `${service_name}.yaml`,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
